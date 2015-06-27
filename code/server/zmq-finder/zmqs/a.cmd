88a54db70ae24dfab89e62390868cb3cad2db836
xpyct <xpyct.am.83@gmail.com>
2015-06-27 17:08:53 +0400

    srv side - zmqs cli side - zmqc

---
+++ code/server/zmq-finder/zmqs/a.cmd
@@ -0,0 +1,4 @@
+rem mingw32-g++.exe -L..\..\..\..\..\..\..\Programs\devel\CodeBlocks-13.12\MinGW\lib -o bin\Debug\zmqs.exe obj\Debug\zmqs.o  -lws2_32  ..\zmq\bin\Debug\libzmq.a ..\soda\bin\Debug\libsoda.a
+rem mingw32-g++.exe -o bin\Debug\zmqs.exe obj\Debug\zmqs.o -lws2_32 ..\zmq\bin\Debug\libzmq.a ..\soda\bin\Debug\libsoda.a
+g:\Programs\devel\CodeBlocks-13.12\MinGW\mingwvars.bat
+rem cmd.exe /q
